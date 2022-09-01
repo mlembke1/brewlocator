14baa82e971ec848b35ea5f368a3ce773262a95a
Mitch Lembke <mitchell.lembke@gmail.com>
2022-08-31 23:00:51 -0400

    Working on styling

--- src/components/Brewery/Brewery.tsx
+++ src/components/Brewery/Brewery.tsx
@@ -9,10 +9,16 @@ interface BreweryProps {
 
 function Brewery(props: BreweryProps) {
   return (
-    <Card>
-      Name: { props.brew.name }
-      City: { props.brew.city }
-      State: { props.brew.state }
+    <Card className={styles.Brewery}>
+      <div className={styles['brew-detail-wrapper']}>
+          <div>Name:</div> <div>{ props.brew.name }</div>
+      </div>
+      <div className={styles['brew-detail-wrapper']}>
+          <div>City:</div> <div>{ props.brew.city }</div>
+      </div>
+      <div className={styles['brew-detail-wrapper']}>
+          <div>State:</div> <div>{ props.brew.state }</div>
+      </div>
     </Card>
   )
 }
